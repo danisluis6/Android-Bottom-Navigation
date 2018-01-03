a432f04ad78ba26c277d15b040078620c7103d8e
lorence <lorence@enclave.vn>
2018-01-03 17:28:55 +0700

    Apply libraries

--- app/src/main/java/tutorial/lovecode/ui/activities/MainActivity.java
+++ app/src/main/java/tutorial/lovecode/ui/activities/MainActivity.java
@@ -1,17 +1,9 @@
 package tutorial.lovecode.ui.activities;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 
 import tutorial.lovecode.R;
-import tutorial.lovecode.ui.fragments.ItemOneFragment;
-import tutorial.lovecode.ui.fragments.ItemThreeFragment;
-import tutorial.lovecode.ui.fragments.ItemTwoFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,35 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)
-                findViewById(R.id.navigation);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener
-                (new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        Fragment selectedFragment = null;
-                        switch (item.getItemId()) {
-                            case R.id.action_item1:
-                                selectedFragment = ItemOneFragment.newInstance();
-                                break;
-                            case R.id.action_item2:
-                                selectedFragment = ItemTwoFragment.newInstance();
-                                break;
-                            case R.id.action_item3:
-                                selectedFragment = ItemThreeFragment.newInstance();
-                                break;
-                        }
-                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                        transaction.replace(R.id.frame_layout, selectedFragment);
-                        transaction.commit();
-                        return true;
-                    }
-                });
-
-        //Manually displaying the first fragment - one time only
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_layout, ItemOneFragment.newInstance());
-        transaction.commit();
     }
 }
