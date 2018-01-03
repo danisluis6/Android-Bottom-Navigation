13a368ef4fa7e101f9ab96325f394aef5994f36b
lorence <lorence@enclave.vn>
2018-01-03 16:25:42 +0700

    master

---
+++ app/src/main/java/tutorial/lovecode/ui/fragments/ItemOneFragment.java
@@ -0,0 +1,33 @@
+package tutorial.lovecode.ui.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import tutorial.lovecode.R;
+
+public class ItemOneFragment extends Fragment {
+
+    private static ItemOneFragment mInstance;
+
+    public static ItemOneFragment newInstance() {
+        if (mInstance == null) {
+            mInstance = new ItemOneFragment();
+        }
+        return mInstance;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_item_one, container, false);
+    }
+
+}
